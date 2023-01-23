33ded0b13b13652145f5b637a1799efb323acb9c
DennisWithot <dennis.withot1@gmail.com>
2023-01-23 21:13:41 +0100

    added intervall.h

---
+++ intervall.h
@@ -0,0 +1,15 @@
+using namespace std;
+
+class Intervall{
+	private:
+	int laenge_Intervall;
+
+	public:
+	//Getter:
+	int getlaenge_Intervall();
+
+	//Setter:
+	void setlaenge_Intervall(int laenge_Intervall);
+
+	Intervall(int laenge_Intervall);
+};
